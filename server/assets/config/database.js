fca8ddaa46a2e817814d5333a8206e6afb4b0ebe
Antoine Plagnol <32707818+plagnol@users.noreply.github.com>
2019-06-09 15:23:20 +0200

    update database connection string

--- server/assets/config/database.js
+++ server/assets/config/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('disc', 'root', '', {
-    host: 'localhost',
+    host: '192.168.0.36',
     dialect: 'mysql',
     operatorsAliases: false,
 
