ddc32ab069800e632ff2d32487e29c5dcb6e76a1
Antoine Plagnol <32707818+plagnol@users.noreply.github.com>
2019-06-09 16:35:08 +0200

    Update database.js

--- server/assets/config/database.js
+++ server/assets/config/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('disc', 'antoine', 'password', {
-    host: 'localhost',
+    host: '192.168.0.25',
     dialect: 'mysql',
     operatorsAliases: false,
 
